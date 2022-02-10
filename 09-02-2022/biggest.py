5c146125788d6edc9143547017fd5b6540afccda
CharvyJain <69421337+CharvyJain@users.noreply.github.com>
2022-02-10 16:59:33 +0530

    Create biggest.py

---
+++ 09-02-2022/biggest.py
@@ -0,0 +1,9 @@
+# Write a program to find biggest of 2 numbers using if-else
+
+num1 = int(input("Enter 1st Number:")
+num2 = int(input("Enter 2nd Number:")
+if num1>num2:
+           print("1st number if biggest.")
+else:
+           print("2nd number if biggest.")
+           
