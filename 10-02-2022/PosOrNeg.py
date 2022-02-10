0b17d7f738292fc2fe4c5cf2f6611cf0f8a2fccd
CharvyJain <69421337+CharvyJain@users.noreply.github.com>
2022-02-10 17:15:17 +0530

    Create PosOrNeg.py

---
+++ 10-02-2022/PosOrNeg.py
@@ -0,0 +1,7 @@
+# write a program to print whether the number is positive or negative.
+
+num = int(input("Enter the number:"))
+if num>0:
+  print(num, "is positive number")
+else:
+  print(num,"is negative number")
