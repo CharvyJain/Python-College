14b53180144446a5e8210cac6f4b6c8b43e2e96c
CharvyJain <69421337+CharvyJain@users.noreply.github.com>
2022-05-07 23:25:45 +0530

    Create Example.py

---
+++ Phase-2/07-05-2022/Example.py
@@ -0,0 +1,3 @@
+https://forms.gle/RrX5kDvyKi4bGvqT8
+
+  Ass1:Write a program to store no for odd keys and yes for even numbers and display them, ex:{1:no,2:yes,3:no,4:yes},Ass2:Write a program to store yes for perfact square keys and no for other from 1 to 1000,ex:{1:yes,2:no,3:no,4:yes}
