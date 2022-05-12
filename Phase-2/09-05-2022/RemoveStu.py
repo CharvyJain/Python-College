b0502a941b7468aa1a2c34f6634aed8c75adc1ec
CharvyJain <69421337+CharvyJain@users.noreply.github.com>
2022-05-12 22:38:31 +0530

    Update RemoveStu.py

--- Phase-2/09-05-2022/RemoveStu.py
+++ Phase-2/09-05-2022/RemoveStu.py
@@ -1,1 +1,13 @@
 # Write a program to remove student details in dictionary.
+
+
+stu = {
+    101: ["Charvy Jain","Female","MSCS","Pass"],
+    102: ["Sita","Female","MPCS","Fail"],
+    103: ["Aditya","Male","MECS","Pass"],
+    104: ["Karthik","Male","CS","Pass"]
+}
+
+x = int(input("Enter Student's Number to delete:"))
+stu.pop(x)
+print(stu)
