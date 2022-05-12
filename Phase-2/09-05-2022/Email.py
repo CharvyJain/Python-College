d9748a20e660ff2154663209a25285e3b1e8b869
CharvyJain <69421337+CharvyJain@users.noreply.github.com>
2022-05-12 22:48:59 +0530

    Update Email.py

--- Phase-2/09-05-2022/Email.py
+++ Phase-2/09-05-2022/Email.py
@@ -1,2 +1,16 @@
 # Write a program to retrieve email id with the name using dictionary.
 
+n = int(input())
+dic = {}
+for i in range(n):
+  x = input("Enter details:").split()
+  dic[x[0]] = x[1]
+while True:
+   try:
+      name = input("Enter the name:")
+      if name in dic:
+         print("Name:",name,"Email:",dic[name],sep=' ')
+      else: print("Not Found")
+   except:
+       break
+  
