a20240bc05ecf70c37afb59554a27425da613ac8
CharvyJain <69421337+CharvyJain@users.noreply.github.com>
2022-05-12 23:46:53 +0530

    Update LC_Paranthesis.py

--- Phase-2/12-05-2022/LC_Paranthesis.py
+++ Phase-2/12-05-2022/LC_Paranthesis.py
@@ -1,1 +1,13 @@
 # https://leetcode.com/problems/valid-parentheses/
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        while(True):
+            if'[]' in s:
+                s=s.replace('[]','')
+            elif '()' in s:
+                s=s.replace('()','')
+            elif '{}' in s:
+                s=s.replace('{}','')
+            else:
+                return not s
